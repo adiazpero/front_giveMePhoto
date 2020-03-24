2e4a46ceab1618e4a3cfd7ccc152d073795b6942
adiazpero <anitta_83@hotmail.com>
2020-03-24 18:53:02 +0100

    metodos realizar pedido, enlace con angular y bbdd de realizar pedido

--- proyecto/src/app/carrito.service.ts
+++ proyecto/src/app/carrito.service.ts
@@ -1,29 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CarritoService {
 
   arrCarrito: any[];
+  baseUrl: string;
 
-  constructor() {
+
+  constructor(private httpClient: HttpClient) {
+
+    this.baseUrl = "http://localhost:3000/api/pedidos/carrito";
+
+
     if (localStorage.getItem('carritoPedido')) {
       this.arrCarrito = JSON.parse(localStorage.getItem('carritoPedido'));
     } else {
       this.arrCarrito = [];
     }
-    console.log(this.arrCarrito)
   }
+
+
 
   recuperarCarrito() {
     return this.arrCarrito
   }
 
+
+
   agregarProducto(pProducto) {
     this.arrCarrito.push(pProducto)
     localStorage.setItem('carritoPedido', JSON.stringify(this.arrCarrito));
   }
+
+
 
   borrarProducto(pId) {
     for (let i = 0; i < this.arrCarrito.length; i++) {
@@ -31,8 +44,17 @@ export class CarritoService {
         this.arrCarrito.splice(i, 1);
         localStorage.setItem('carritoPedido', JSON.stringify(this.arrCarrito));
       }
-    }
+    };
   }
+
+
+
+
+  enviarPedido(pProductoId) {
+    return this.httpClient.post(`${this.baseUrl}/carrito`, pProductoId).toPromise();
+  }
+
+
 
 
 }
--- proyecto/src/app/carrito/carrito.component.ts
+++ proyecto/src/app/carrito/carrito.component.ts
@@ -1,44 +1,73 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CarritoService } from '../carrito.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
+
+
 export class CarritoComponent implements OnInit {
 
-  /* 
-    @Input() */
   listaPedido: any[];
+  formulario: FormGroup;
+  mostrar: boolean;
 
   constructor(private carritoService: CarritoService) {
 
+    this.mostrar = false;
+
+    this.formulario = new FormGroup({
+      direccion: new FormControl(''),
+      telefono: new FormControl('')
+    });
   }
+
+
 
   ngOnInit() {
     /*     this.listaPedido = JSON.parse(localStorage.getItem('pedido'));
         console.log(this.listaPedido); */
     this.listaPedido = this.carritoService.recuperarCarrito();
-
   }
 
 
-  /*   anyFunction() {
-      console.log('anyfunction en el carrito')
-    } */
+
+  onSubmit() {
+    console.log(this.formulario.value)
+  }
 
   manejarBorrar(pId) {
     this.carritoService.borrarProducto(pId);
   }
 
+
+
   sumaProductos() {
     let resultado = 0;
     for (const producto of this.listaPedido) {
-      console.log('listaPedido:', this.listaPedido)
+      // console.log('listaPedido:', this.listaPedido)
       resultado += producto.precio;
-      console.log('resultado suma', resultado)
+      // console.log('resultado suma', resultado)
     }
     return resultado;
   }
+
+
+
+
+  realizarPedido() {
+    for (let producto of this.listaPedido) {
+      console.log(producto.id)
+      this.carritoService.enviarPedido(producto.id);
+      this.mostrar = !this.mostrar;
+    }
+  }
+
+
+
+
+
 }
