887ae88110149e4f583fa818c1a22d131bc46e0d
adiazpero <anitta_83@hotmail.com>
2020-03-26 12:40:13 +0100

    desplegable de perfil/login y modificaciones css

--- proyecto/src/assets/js/funciones.js
+++ proyecto/src/assets/js/funciones.js
@@ -2,7 +2,7 @@ function launchNavbar() {
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        if (scroll < 150) {
+        if (scroll < 100) {
             $('.fixed-top').css('background', 'transparent');
         } else {
             $('.fixed-top').css('background', 'white');
